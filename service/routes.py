9924b94eddf2c868d0c66ea678a571d8f749b003
AugieP57 <167009114+AugieP57@users.noreply.github.com>
2024-05-07 14:17:30 -0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -82,11 +82,22 @@ def get_accounts(account_id):
     return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
-# UPDATE AN EXISTING ACCOUNT
+# LIST ALL ACCOUNTS
 ######################################################################
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """
+    List all Accounts
+    This endpoint will list all Accounts
+    """
+    app.logger.info("Request to list Accounts")
 
-# ... place you code here to UPDATE an account ...
+    # use the Account.all() method to retrieve all accounts
+    # create a list of serialize() accounts
+    # log the number of accounts being returned in the list 
+    # return the list with a return code of status.HTTP_200_OK
 
+    return {list of accounts as json here + 200}
 
 ######################################################################
 # DELETE AN ACCOUNT
