1895318b739e75976c60f4f726a84e38fc3a74fd
AugieP57 <167009114+AugieP57@users.noreply.github.com>
2024-05-07 16:27:49 -0500

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -20,6 +20,20 @@ DATABASE_URI = os.getenv(
 BASE_URL = "/accounts"
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
+def test_security_headers(self):
+  """It should return security headers"""
+  response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+  self.assertEqual(response.status_code, status.HTTP_200_OK)
+  headers = {
+    'X-Frame-Options': 'SAMEORIGIN',
+    'X-Content-Type-Options': 'nosniff',
+    'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+    'Referrer-Policy': 'strict-origin-when-cross-origin'
+  }
+  
+  for key, value in headers.items():
+    self.assertEqual(response.headers.get(key), value)
+
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
