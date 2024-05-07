bb0f6d3fe528586bde7c37493f24c7c3e6e98f2a
AugieP57 <167009114+AugieP57@users.noreply.github.com>
2024-05-07 14:20:12 -0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -61,7 +61,6 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
-# ... place you code here to LIST accounts ...
 
 
 ######################################################################
@@ -80,24 +79,26 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
-
+    
 ######################################################################
-# LIST ALL ACCOUNTS
+# UPDATE AN EXISTING ACCOUNT
 ######################################################################
-@app.route("/accounts", methods=["GET"])
-def list_accounts():
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
     """
-    List all Accounts
-    This endpoint will list all Accounts
+    Update an Account
+    This endpoint will update an Account based on the posted data
     """
-    app.logger.info("Request to list Accounts")
+    app.logger.info("Request to update an Account with id: %s", account_id)
 
-    # use the Account.all() method to retrieve all accounts
-    # create a list of serialize() accounts
-    # log the number of accounts being returned in the list 
-    # return the list with a return code of status.HTTP_200_OK
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
-    return {list of accounts as json here + 200}
+        account.deserialize(request.get_json())
+        account.update()
+
+    return account.serialize(), status.HTTP_200_OK
 
 ######################################################################
 # DELETE AN ACCOUNT
