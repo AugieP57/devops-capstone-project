c5e1bb00cf0d91b432087b1067e2a71ce4a51594
AugieP57 <167009114+AugieP57@users.noreply.github.com>
2024-05-07 16:45:17 -0500

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -11,6 +11,15 @@ from service.common import log_handlers
 from flask_talisman import Talisman
 from flask_cors import CORS
 
+def test_cors_security(self):
+    """It should return a CORS header"""
+    response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    # Check for the CORS header
+    self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+
+CORS(app)
+
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
